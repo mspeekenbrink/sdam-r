e7b565cb07ee0558601ddde827204f9dfc296491
Maarten <maarten.speekenbrink@gmail.com>
2020-07-24 01:13:31 +0100

    data sets

---
+++ R/data.R
@@ -0,0 +1,29 @@
+#' Predictions by Paul the Octopus in the 2008 UEFA Cup.
+#'
+#' A dataset containing the predictions and outcomes of matches
+#' in the 2008 UEFA European Cup.
+#'
+#' @format A data frame with 6 rows and 4 variables:
+#' \describe{
+#'   \item{Match}{countries playing}
+#'   \item{Prediction}{country predicted to win}
+#'   \item{Result}{score at the end of the match}
+#'   \item{Outcome}{whether Paul was correct or incorrect}
+#' }
+#' @source \url{https://en.wikipedia.org/wiki/Paul_the_Octopus}
+"uefa2008"
+
+#' Predictions by Paul the Octopus in the 2010 Fifa World Cup.
+#'
+#' A dataset containing the predictions and outcomes of matches
+#' in the 2010 FIFA European Cup.
+#'
+#' @format A data frame with 8 rows and 4 variables:
+#' \describe{
+#'   \item{Match}{countries playing}
+#'   \item{Prediction}{country predicted to win}
+#'   \item{Result}{score at the end of the match}
+#'   \item{Outcome}{whether Paul was correct or incorrect}
+#' }
+#' @source \url{https://en.wikipedia.org/wiki/Paul_the_Octopus}
+"fifa2010"
