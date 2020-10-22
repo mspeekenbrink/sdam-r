acfd0b0de61658358b1c5c5a798b921802cb7ee3
Maarten Speekenbrink <4246553+mspeekenbrink@users.noreply.github.com>
2020-10-22 15:35:01 +0100

    added legacy and speeddate data

--- R/data.R
+++ R/data.R
@@ -70,7 +70,7 @@
 #' @source  \url{https://osf.io/pqf9r/}. See also Klein, R. A., Ratliff, K. A., Vianello, M., Adams, R. B., Jr., Bahník, Š., Bernstein, M. J., . . ., Nosek, B. A. (2014). Investigating variation in replicability: A "many labs" replication project. Social Psychology, 45(3), 142-152. \url{http://dx.doi.org/10.1027/1864-9335/a000178}
 "anchoring"
 
-#' Trump votes in 2016 for 51 US states
+#' Trump votes in 2016 for 50 US states and the District of Columbia
 #'
 #'
 #' @format A data frame with 4632 rows and 5 variables:
@@ -85,4 +85,74 @@
 #' }
 #' @source CSI Without Dead Bodies "Hate Groups and Trump's Vote%: Predictive effect present when education and poverty are considered" \url{https://www.csiwithoutdeadbodies.com/2017/02/hate-groups-and-trumps-vote-predictive.html}
 "trump2016"
+
+#' Legacy motives and pro-environmental behaviour
+#' 
+#' 
+#' @format A data frame with 245 rows and 9 variables:
+#' \describe{
+#'   \item{id}{(numeric) ID variable relating to the original dataset}
+#'   \item{sex}{(character) biological sex of participant (male or female)}
+#'   \item{age}{(numeric) age in years}
+#'   \item{legacy}{(numeric) Sverage of 8 items reflecting legacy motivation, on a scale from 1 (Not at all) to 6 (A great amount)}
+#'   \item{belief}{(numeric) average of 5 items reflecting belief in climate change, on a scale from 1 (Strongly Disagree) to 7 (Strongly Agree)}
+#'   \item{intention}{(numeric) average of 8 items reflecting intention to act in a pro-environmental way, on a scale from 1 (Never) to 6 (All the time)}
+#'   \item{education}{(numeric) Level of education, 1 = 8th grade or less, 2 = Some high school, 3 = Graduated high school, 4 = Some college or technical school, 5 = Graduated college or technical school, 6 = Post-graduate}
+#'   \item{income}{(numeric) Approximate household income, 1 = less than $20K, 2 = $20K-$35K, 3 = $35K-$50K 4 = $50K-$75K, 5 = $75K-100K, 6 = more than 100K}
+#'   \item{donation}{(numeric) Donation of possible bonus payment, between $0 and $10}
+#' }
+#' 
+#' @examples
+#' \dontrun{
+#'  # this dataset was processed from the raw data as follows:
+#'  tdat <- read.csv("legacy study - pilot_data.csv")
+#`  dat <- data.frame(
+#`   id = tdat$id,
+#`   sex = ifelse(tdat$Male,"male","female"),
+#`   age = 2015 - tdat$YOB,
+#`   legacy = rowMeans(tdat[,3:10]),
+#`   belief = rowMeans(tdat[,12:16]),
+#`   intention = rowMeans(tdat[,17:23]),
+#`   education = as.numeric(tdat$educ),
+#`   income = tdat$Income,
+#`   donation = tdat$donate_dv_1)
+#`  dat$legacy <- round(dat$legacy,2)
+#`  dat$belief <- round(dat$belief,2)
+#`  dat$intention <- round(dat$intention,2)
+#`  dat <- dat[!is.na(rowSums(dat[,4:9])),]
+#`  legacy2015 <- dat
+#' }
+#' @source Harvard DataVerse \url{https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/27740&version=1.0}
+"legacy2015"
+
+#' Speed dating
+#' 
+#' A subset of cases (wave 6-9) and variables (see below) from an experiment on speed dating. 
+#' by Columbia Business School professors Ray Fisman and Sheena Iyengar for their paper 
+#' Gender Differences in Mate Selection: Evidence From a Speed Dating Experiment.
+#' 
+#' @format A data frame with 1562 rows and 32 variables:
+#' \describe{
+#'   \item{iid}{(numeric) unique ID variable of participant}
+#'   \item{pid}{(numeric) unique ID variable of date partner}
+#'   \item{age}{(numeric) age in years}
+#'   \item{gender}{(character) gender of participant (male or female)}
+#'   \item{self_like}{(numeric) how much they like their date partner in general (between  1 and 10)}
+#'   \item{other_like}{(numeric) how much their date partner likes them (between  1 and 10)}
+#'   \item{self_want}{do they want to go on another date with their date partner? (1 = yes, 0 = no)}
+#'   \item{other_want}{does their date partner want to go on another date with them? (1 = yes, 0 = no)}
+#'   \item{match}{do they both want to go on another date with each other? (1 = yes, 0 = no)}
+#'   \item{self_attr}{how attractive do they think they are? (between 1 and 10)}
+#'   \item{self_sinc}{how sincere do they think they are? (between 1 and 10)}
+#'   \item{self_intel}{how intelligent do they think they are? (between 1 and 10)}
+#'   \item{self_fun}{how much fun do they think they are? (between  1 and 10)}
+#'   \item{self_amb}{how ambitious do they think they are? (between 1 and 10)}
+#'   \item{other_attr,other_sinc,other_intel,other_fun,other_amb}{how attractive etc does their date partner think they are? (between  1 and 10)}
+#'   \item{other_shar}{how much does their date partner think they share hobbies and interests? (between 1 and 10)}
+#'   \item{date_attr,date_sinc,date_intel,date_fun,date_amb,date_shar}{how do they rate their date partner's attractiveness etc? (between 1 and 10)}
+#'   \item{self_imp_attr,self_imp_sinc,self_imp_intel,self_imp_fun,self_imp_amb,self_imp_shar}{how important do they find attractiveness etc in a partner? (between 1 and 10)}
+#'   \item{other_imp_attr,other_imp_sinc,other_imp_intel,other_imp_fun,other_imp_amb,other_imp_shar}{how important does their date partner find attractiveness etc? (between 1 and 10)}
+#' }
+#' @source Kaggle \url{https://www.kaggle.com/annavictoria/speed-dating-experiment}
+"speeddate"
 
---
+++ helpers/speeddate.R
@@ -0,0 +1,56 @@
+library(dplyr)
+
+tdat <- read.csv("helpers/Speed Dating Data.csv")
+tdat <- subset(tdat,wave %in% 6:9)
+dat <- tdat %>% group_by(iid, pid) %>% summarise(
+  age = first(age),
+  gender = first(ifelse(gender == 1, "male", "female")),
+  
+  self_like = mean(like, na.rm = TRUE),
+  other_like = mean(like_o, na.rm = TRUE),
+  self_want = mean(dec, na.rm = TRUE),
+  other_want = mean(dec_o, na.rm = TRUE),
+
+  match = mean(match, na.rm = TRUE),
+  
+  self_attr = first(attr3_1),
+  self_sinc = first(sinc3_1),
+  self_intel = first(intel3_1),
+  self_fun = first(fun3_1),
+  self_amb = first(amb3_1),
+  #self_shar = mean(shar3_1),
+  
+  other_attr = mean(attr_o, na.rm = TRUE),
+  other_sinc = mean(sinc_o, na.rm = TRUE),
+  other_intel = mean(intel_o, na.rm = TRUE),
+  other_fun = mean(fun_o, na.rm = TRUE),
+  other_amb = mean(amb_o, na.rm = TRUE),
+  other_shar = mean(shar_o, na.rm = TRUE),
+  
+  date_attr = mean(attr, na.rm = TRUE),
+  date_sinc = mean(sinc, na.rm = TRUE),
+  date_intel = mean(intel, na.rm = TRUE),
+  date_fun= mean(fun, na.rm = TRUE),
+  date_amb = mean(amb, na.rm = TRUE),
+  date_shar = mean(shar, na.rm = TRUE),
+  
+  self_imp_attr = first(attr1_1),
+  self_imp_sinc = first(sinc1_1),
+  self_imp_intel = first(intel1_1),
+  self_imp_fun = first(fun1_1),
+  self_imp_amb = first(amb1_1),
+  self_imp_shar = first(shar1_1),
+  
+  other_imp_attr = mean(pf_o_att, na.rm=TRUE),
+  other_imp_sinc = mean(pf_o_sin, na.rm=TRUE),
+  other_imp_intel = mean(pf_o_int, na.rm = TRUE),
+  other_imp_fun = mean(pf_o_fun, na.rm = TRUE),
+  other_imp_amb = mean(pf_o_amb, na.rm = TRUE),
+  other_imp_shar = mean(pf_o_sha, na.rm = TRUE)
+  
+) %>% 
+  mutate_all(funs(ifelse(is.nan(.), NA, .)))
+
+speeddate <- as.data.frame(dat)
+
+save(speeddate, file="data/speeddate.rda")
